08c8ffed39808c692eba0edcc86aec5cbf2b885b
Gambini23 <101364639+Gambini23@users.noreply.github.com>
2025-05-27 20:21:55 +0200

    Update macro_portion_calculator_streamlit.py

--- macro_portion_calculator_streamlit.py
+++ macro_portion_calculator_streamlit.py
@@ -100,6 +100,7 @@ raccomanda di rivolgersi a professionisti abilitati ai sensi della normativa vig
 """)
 
     pdf = FPDF()
+    pdf.add_font('DejaVu', 'B', '/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', uni=True)
     pdf.add_font('DejaVu', '', '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', uni=True)
     pdf.add_page()
     pdf.set_font("DejaVu", 'B', 14)
