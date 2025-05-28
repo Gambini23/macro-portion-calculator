f7826f2c0357320808dcdbb5bae941e588e9e8b1
Gambini23 <101364639+Gambini23@users.noreply.github.com>
2025-05-28 12:04:55 +0200

    Update food_data.py

--- macro_meal_planner/food_data.py
+++ macro_meal_planner/food_data.py
@@ -1,5 +1,4 @@
 FOODS_COLAZIONE = {
-   FOODS_COLAZIONE = {
     "Cornflakes": {"carbs": 84, "kcal": 370},
     "Pane integrale": {"carbs": 40, "kcal": 230},
     "Fiocchi d'avena": {"carbs": 60, "kcal": 370},
@@ -23,8 +22,6 @@ FOODS_COLAZIONE = {
     "Cioccolato fondente": {"fat": 35, "kcal": 550},
 }
 
-
-
 FOODS_PASTI = {
     "Pasta integrale": {"carbs": 70, "kcal": 350},
     "Riso": {"carbs": 78, "kcal": 360},
@@ -32,21 +29,22 @@ FOODS_PASTI = {
     "Patate crude": {"carbs": 17, "kcal": 80},
     "Gnocchi di patate": {"carbs": 30, "kcal": 150},
     "Pane integrale": {"carbs": 40, "kcal": 230},
-"Piadina confezionata": {"carbs": 50, "kcal": 305},
+    "Piadina confezionata": {"carbs": 50, "kcal": 305},
     "Carne bianca": {"protein": 22, "kcal": 120},
     "Carne rossa": {"protein": 26, "kcal": 250},
     "Pesce bianco": {"protein": 20, "kcal": 137},
     "Pesce grasso": {"protein": 20, "kcal": 200},
     "Tofu": {"protein": 15, "kcal": 145},
-"Uova": {"protein": 13, "kcal": 143, "unit": 60},
-"Albume": {"protein": 11, "kcal": 52},
-"Mozzarella light": {"protein": 22, "kcal": 170},
+    "Uova": {"protein": 13, "kcal": 143, "unit": 60},
+    "Albume": {"protein": 11, "kcal": 52},
+    "Mozzarella light": {"protein": 22, "kcal": 170},
     "Bresaola": {"protein": 32, "kcal": 151},
-"Prosciutto cotto": {"protein": 20, "kcal": 145},
-"Prosciutto crudo": {"protein": 28, "kcal": 215},
-"Ricotta light": {"protein": 11, "kcal": 136},
+    "Prosciutto cotto": {"protein": 20, "kcal": 145},
+    "Prosciutto crudo": {"protein": 28, "kcal": 215},
+    "Ricotta light": {"protein": 11, "kcal": 136},
     "Olio EVO": {"fat": 100, "kcal": 900},
-"Avocado": {"fat": 15, "kcal": 160},
+    "Avocado": {"fat": 15, "kcal": 160},
 }
 
 KCAL_PER_GRAM = {"carbs": 4, "protein": 4, "fat": 9}
+
